20ca6af68c73d2acc6091aa385d8f4a12d7f5f71
celine.john.1424 <celine.john.1424@gmail.com>
2024-01-25 14:12:04 +0400

    first phase

--- main.js
+++ main.js
@@ -1,3 +1,38 @@
+function typeEffect(element, container, text, delay = 100) {
+  let textArray = text.split('');
+  let currentIndex = 0;
+  element.innerHTML = '';
+  container.style.opacity = 1; // Make the container element visible
+
+  let typeTimer = setInterval(function() {
+      if (currentIndex < textArray.length) {
+          element.append(textArray[currentIndex]);
+          currentIndex++;
+      } else {
+          clearInterval(typeTimer);
+      }
+  }, delay);
+}
+
+// Apply the typing effect with delays
+window.onload = function() {
+  const helloText = document.getElementById('hello');
+  const celineText = document.getElementById('celine');
+  const dataScientistText = document.getElementById('data-scientist');
+  const containerHello = document.getElementById('container-hello');
+  const containerCeline = document.getElementById('container-celine');
+  const containerDataScientist = document.getElementById('container-data-scientist');
+
+  const helloTextDelay = helloText.textContent.length * 100;
+  const celineTextDelay = celineText.textContent.length * 100;
+
+  typeEffect(helloText, containerHello, helloText.textContent, 100);
+  setTimeout(() => typeEffect(celineText, containerCeline, celineText.textContent, 100), helloTextDelay + 100); // 1s after helloText
+  setTimeout(() => typeEffect(dataScientistText, containerDataScientist, dataScientistText.textContent, 100), helloTextDelay + celineTextDelay + 1000); // 1s after celineText
+};
+
+
+
 function adjustCardHeight() {
     var cards = document.querySelectorAll('.mini-project-card');
     cards.forEach(function(card) {
@@ -11,4 +46,6 @@ function adjustCardHeight() {
   
   // Adjust height on page load
   adjustCardHeight();
+
+
   
